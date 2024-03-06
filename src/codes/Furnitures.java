ba39e5929b32ca39fae93365b05ef899db749da5
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-03-06 08:33:35 -0800

    'added argument exceptions for Furnitures class and its child classes'

--- src/codes/Furnitures.java
+++ src/codes/Furnitures.java
@@ -3,6 +3,9 @@ public class Furnitures extends HomeObjects
     private final boolean isAssemblyRequired;
     private final String  frameMaterial;
 
+    private static final String FRAME_WOOD = "wood";
+    private static final String FRAME_METAL = "metal";
+
     public Furnitures(final String  homeObjectName,
                       final String  homeObjectBrand,
                       final double  priceCAD,
@@ -11,6 +14,15 @@ public class Furnitures extends HomeObjects
                       final String  frameMaterial)
     {
         super(homeObjectName, homeObjectBrand, priceCAD, homeObjectColor);
+
+        if(frameMaterial != null &&
+                (!frameMaterial.equalsIgnoreCase(FRAME_WOOD)
+                        && !frameMaterial.equalsIgnoreCase(FRAME_METAL))
+        )
+        {
+            throw new IllegalArgumentException("Invalid frame material: " + frameMaterial);
+        }
+
         this.isAssemblyRequired = isAssemblyRequired;
         this.frameMaterial      = frameMaterial;
     }
--- src/codes/Sofa.java
+++ src/codes/Sofa.java
@@ -3,6 +3,11 @@ public class Sofa extends Furnitures
     private final int seatingCapacity;
     private final String upholstery;
 
+    private static final int MIN_SEATING = 1;
+    private static final int MAX_SEATING = 16;
+    private static final String MATERIAL_VELVET = "velvet";
+    private static final String MATERIAL_LEATHER = "leather";
+
     public Sofa(final String homeObjectName,
                 final String homeObjectBrand,
                 final double priceCAD,
@@ -13,6 +18,20 @@ public class Sofa extends Furnitures
                 final String upholstery)
     {
         super(homeObjectName, homeObjectBrand, priceCAD, homeObjectColor, isAssemblyRequired, frameMaterial);
+
+        if(seatingCapacity < MIN_SEATING || seatingCapacity > MAX_SEATING)
+        {
+            throw new IllegalArgumentException("Invalid seating capacity: " + seatingCapacity);
+        }
+
+        if(upholstery != null &&
+                (!upholstery.equalsIgnoreCase(MATERIAL_LEATHER)
+                        && !upholstery.equalsIgnoreCase(MATERIAL_VELVET))
+        )
+        {
+            throw new IllegalArgumentException("Invalid frame material: " + frameMaterial);
+        }
+
         this.seatingCapacity = seatingCapacity;
         this.upholstery = upholstery;
     }
