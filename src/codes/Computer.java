f9885dd3a7f16f7d519ae57011c320261e5c4f89
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-03-06 01:10:31 -0800

    'added Exceptions'

--- src/codes/Computer.java
+++ src/codes/Computer.java
@@ -1,7 +1,12 @@
 public class Computer extends Electronics
 {
-    private final String processor;
-    private final int memoryGb;
+    private final String    processor;
+    private final long       memoryGb;
+
+    private static final int MIN_PROCESSOR_NAME = 4;
+    private static final int MAX_PROCESSOR_NAME = 12;
+    private static final long MIN_MEMORY_GB = 8;
+    private static final long MAX_MEMORY_GB = 17_000_000_000L;
 
     public Computer(final String homeObjectName,
                     final String homeObjectBrand,
@@ -10,9 +15,25 @@ public class Computer extends Electronics
                     final int voltageV,
                     final String itemConnectivity,
                     final String processor,
-                    final int memoryGb)
+                    final long memoryGb)
     {
         super(homeObjectName, homeObjectBrand, priceCAD, homeObjectColor, voltageV, itemConnectivity);
+
+        if(processor == null)
+        {
+                throw new NullPointerException("Invalid item processor: null");
+        }
+
+        if(processor.length() < MIN_PROCESSOR_NAME || processor.length() > MAX_PROCESSOR_NAME)
+        {
+            throw new IllegalArgumentException("Invalid memory: " + processor);
+        }
+
+        if(memoryGb < MIN_MEMORY_GB || memoryGb > MAX_MEMORY_GB)
+        {
+            throw new IllegalArgumentException("Invalid memory: " + memoryGb);
+        }
+
         this.processor = processor;
         this.memoryGb = memoryGb;
     }
